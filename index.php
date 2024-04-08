2f1501b3c88859a34e08113d420feef4b319b7fb
Fanasina Ramalandimanana <hope.fanasina2@gmail.com>
2024-04-08 14:20:36 +0300

    BEGIN

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="./images/spacecup_favicon.png" type="image/x-icon">
+  <link rel="stylesheet" href="./stylesheets/style.css">
+  <title>SPACECUP | Home</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="bg-filter-layer"></div>
+    <div class="bg-img"></div>
+  </div>
+  <header>
+
+  </header>
+  <main>
+    <section class="hero">
+      
+    </section>
+  </main>
+</body>
+
+</html>
